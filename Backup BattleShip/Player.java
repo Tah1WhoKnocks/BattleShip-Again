58f066690d57bef4380e8c9d06a353e04c19e183
Tah1WhoKnocks <teajaylander@gmail.com>
2017-03-03 09:37:21 -0600

    Project as it was Wednesday night

---
+++ Backup BattleShip/Player.java
@@ -0,0 +1,191 @@
+import java.util.Arrays;
+
+public class Player {
+	private int turn;
+	
+	//0 is hit ship
+	//1 is no ship
+	//>1 is a ship
+	private int[][] ships;
+	
+	
+	//0 is hit ship
+	//1 is missed ship
+	//2 is unknown
+	private int[][] enemies;
+	
+	public Player(int i)
+	{
+		 ships = new int[][]{ {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1} };
+		 enemies = new int[][] { {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2} };
+		turn = i;
+		
+	}
+	
+	public Player(int i,int[][] v)
+	{
+		 turn = i; 
+		 ships = v;
+		 enemies = new int[][] { {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2},
+			 {2,2,2,2,2,2,2,2,2,2} };
+		
+		
+	}
+	
+	
+	//Self Explanatory
+	public int[][] getShips()
+	{
+		return ships;
+	}
+	
+	public int[][] getEnemies()
+	{
+		return enemies;
+	}
+	public int getTurn()
+	{
+		return turn;
+	}
+	
+	
+	//Here we have the attack method, it uses the receive attack method, and changes the two arrays
+	//Whether or not there is a ship.
+	
+	public boolean attack(Player p, int x, int y)
+	{
+		y-=1;
+		if(p.recieveAttack(x, y))
+		{
+			enemies[x][y] = 0;
+			return true;
+		}else{
+			enemies[x][y] = 1;
+			return false;
+		}
+	}
+   
+   public boolean recieveAttack(int x, int y)
+	{
+      //y-=1;
+		if(ships[x][y]>1)
+		{
+			ships[x][y] = 0;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public int convert(String s)
+	{
+		int row;
+			if(s.toLowerCase().equals("a"))
+			{
+				row = 0;
+			}else if(s.toLowerCase().equals("b")){
+				row = 1;
+			}else if(s.toLowerCase().equals("c")){
+				row =2;
+			}else if(s.toLowerCase().equals("d")){
+				row = 3;
+			}else if(s.toLowerCase().equals("e")){
+				row = 4;
+			}else if(s.toLowerCase().equals("f")){
+				row = 5;
+			}else if(s.toLowerCase().equals("g")){
+				row = 6;
+			}else if(s.toLowerCase().equals("h")){
+				row = 7;
+			}else if(s.toLowerCase().equals("i")){
+				row = 8;
+			}else{
+				row = 9;
+			}
+				
+		return row;
+	}
+	
+	//Builds the text grid, eventually will implement swing.
+	public String printMe()
+	{
+      String[] rows = {"A","B","C","D","E","F","G","H","I","J"};
+		String full = "   1 2 3 4 5 6 7 8 9 10";
+		for(int a = 0;a<ships.length;a++)
+		{
+			if (a<rows.length)
+			{
+				full+="\n"+(rows[a])+"  ";	
+			}
+			
+			for(int b = 0;b<ships[a].length;b++)
+			{
+				full+= ships[a][b]+" ";
+			}
+		}
+		return full;
+	}
+	
+	public String printEnemies()
+	{
+		String[] rows = {"A","B","C","D","E","F","G","H","I","J"};
+		String full = "   1 2 3 4 5 6 7 8 9 10";
+		for(int a = 0;a<enemies.length;a++)
+		{
+			if (a<rows.length)
+			{
+				full+="\n"+(rows[a])+"  ";	
+			}
+			
+			for(int b = 0;b<enemies[a].length;b++)
+			{
+				full+= enemies[a][b]+" ";
+			}
+		}
+		return full;
+	}
+   
+   public boolean checkWinner(Player p)//returns true if target player has no remaining ships
+   {
+      int count = 0;
+      
+      for(int r = 0; r<p.getShips().length; r++)
+         for(int c = 0; c<p.getShips()[r].length; c++)
+            if(p.getShips()[r][c]>1)
+               count++;
+      
+      if(count==0)
+         return true;
+         
+      return false;
+   }
+	
+	
+}
+
---
+++ Backup BattleShip/ShipRunner.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ShipRunner {
+	public static void main(String[]args)
+	{
+		Scanner scan = new Scanner(System.in);
+		int winner = 0;
+		int turn = ((int)Math.random()+1 *2);
+		int[][] p1ships = new int[][]{ {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,1,1,1},
+			 {1,1,1,1,1,1,1,3,3,3} };
+			  
+			  int[][] p2ships = new int[][]{ {3,3,3,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1},
+					 {1,1,1,1,1,1,1,1,1,1} };
+		
+		Player p1 = new Player(1, p1ships);
+		Player p2 = new Player(2, p2ships);
+      
+      String[] q = {"1+2=","2+2=","3+2=","4+2="};
+      String[] a = {"3","4","5","6"};
+      Quiz addition = new Quiz(q,a);
+		
+		while (winner <1)
+		{
+			String xa = "";
+			int ya = 0;
+			
+			if(turn == 1)
+			{
+            System.out.print("\n\nPlayer "+ turn + "!");
+            if(addition.showTime())
+            {
+               System.out.print("\nCORRECT!");
+				   System.out.print("\n"+p1.printEnemies());
+				   System.out.print("\n\n"+p1.printMe());
+            
+				   System.out.print("\n"+"Choose Your Attacking Row!");
+				   xa = scan.next();
+				   System.out.print("Choose Your Attacking Collumn!");
+				   ya = scan.nextInt();
+				   if(p1.attack(p2, p2.convert(xa), ya))
+				   {
+				   	System.out.print("Hit!\n");
+				   }else{
+				   	System.out.print("Miss!\n");
+				   }
+            }
+            else
+            {
+               System.out.print("\nINCORRECT!");
+            }
+            if(p1.checkWinner(p2))
+            {
+               System.out.print("\n\nPLAYER " + turn + " WINS!!!" + "\nGG!!!");
+               winner = 1;
+            }
+            else
+               turn = 2;
+				
+			}else{
+            System.out.print("\n\nPlayer "+ turn + "!");
+				if(addition.showTime())
+            {
+               System.out.print("\nCORRECT!");
+				   System.out.print("\n"+p2.printEnemies());
+				   System.out.print("\n\n"+p2.printMe());
+            
+				   System.out.print("\n"+"Choose Your Attacking Row!");
+				   xa = scan.next();
+				   System.out.print("Choose Your Attacking Collumn!");
+				   ya = scan.nextInt();
+				   if(p2.attack(p1, p1.convert(xa), ya))
+				   {
+				   	System.out.print("Hit!\n");
+				   }else{
+				   	System.out.print("Miss!\n");
+				   }
+            }
+            else
+            {
+               System.out.print("\nINCORRECT!");
+            }
+            if(p2.checkWinner(p1))
+            {
+               System.out.print("\n\nPLAYER " + turn + " WINS!!!" + "\nGG!!!");
+               winner = 1;
+            }
+            else
+               turn = 1;
+         }
+      }
+      
+   }//ends main
+}//ends class
