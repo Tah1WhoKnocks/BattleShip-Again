7357036add3efad88662837012af31a34d4525fd
Viyi <arthur.gho.howard@gmail.com>
2017-03-07 21:23:43 -0600

    Fixed Boat Maker

--- Backup BattleShip/GuiRunner.java
+++ Backup BattleShip/GuiRunner.java
@@ -16,26 +16,12 @@ public class GuiRunner {
 	    int turn = 1;
 	   
 	  
-		//something like this ok?	
-	    BoatMaker boatMake = new BoatMaker();
-			boatMake.initialize();
-			boatMake.getFrame().setVisible(true);
-			int shiptotal = 0;
-			while(shiptotal<10){
-				for(int c = 0;c <boatMake.getShipShape().length;c++)
-				{
-					for(int b = 0;b<boatMake.getShipShape()[c].length;b++)
-					{
-						if(boatMake.getShipShape()[c][b].isSelected())
-						{
-							p1.setShips(c, b);
-							shiptotal++;
-						}
-					}	
-				}
-			}
-			
-			
+		//sjp
+	   p1.makeBoats();
+	   p2.makeBoats();
+	   
+	   
+	  
 			
 			
 			
--- Backup BattleShip/Player.java
+++ Backup BattleShip/Player.java
@@ -88,6 +88,30 @@ public class Player {
 		}
 	}
    
+	//Make boats!
+	public void makeBoats()
+	{
+		 BoatMaker boatMake = new BoatMaker();
+			boatMake.initialize();
+			boatMake.getFrame().setVisible(true);
+			int shiptotal = 0;
+			while(shiptotal<10){
+				for(int c = 0;c <boatMake.getShipShape().length;c++)
+				{
+					for(int b = 0;b<boatMake.getShipShape()[c].length;b++)
+					{
+						if(boatMake.getShipShape()[c][b].isSelected() && ships[c][b] != 2)
+						{
+							setShips(c, b);
+							shiptotal++;
+						}
+					}	
+				}
+			}
+			
+		
+	}
+	
    public boolean recieveAttack(int x, int y)
 	{
       //y-=1;
